6c78f08cbc85f53a1258a013877f38d224f6c8de
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-02-19 15:44:05 +0300

    Change side bar

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import {CreateTask} from "pages/admin/createTask/CreateTask";
 import {Error404} from "pages/error404/Error404";
 import {ViewTaskModal} from "pages/admin/viewTaskModal/ViewTaskModal";
 import {Settings} from "pages/admin/settings/Settings";
-import Index from "pages/Index";
 
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
         <Route path='/admin/tasks/all' element={<ViewAllTasksDashboard/>}/>
         <Route path='/admin/tasks/all/modal/:id' element={<ViewTaskModal/>}/>
         <Route path='/admin/settings' element={<Settings/>}/>
-        <Route path='/hueta' element={<Index/>}/>
         <Route path={'*'} element={<Error404/>}/>
       </Routes>
     </div>
