1842aa2fb2e1fe4534d753f51eb084b4388b5c53
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-03-06 15:13:53 +0300

    Create ViewAllAccessesDashboard component and ViewAccessModal components

---
+++ src/pages/admin/accesses/viewAccessModal/ViewAccessModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './viewAccessModal.module.css';
+export const ViewAccessModal = () => {
+    return (
+        <div>
+            ViewAccessModal
+        </div>
+    );
+};
---
+++ src/pages/admin/accesses/viewAllAccessesDashboard/ViewAllAccessesDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './viewAllAccessesDashboard.module.css'
+
+export const ViewAllAccessesDashboard = () => {
+    return (
+        <div>
+            ViewAllAccessesDashboard
+        </div>
+    );
+};
+
